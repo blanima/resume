aac638cbcfe409002634983a026596c9e332d35b
blanima <107488191+blanima@users.noreply.github.com>
2025-03-14 11:19:09 +0100

    core/feat: extend UseCase interface
    - changes useCase interface execute method to return a Promise
    - adds type parameter to UseCase type imports

--- packages/core/src/interfaces.ts
+++ packages/core/src/interfaces.ts
@@ -1,6 +1,6 @@
-import { AppError } from "./errors";
-import { Result } from "./monads";
+import { type AppError } from "./errors";
+import { type Result } from "./monads";
 
 export interface UseCase<I, O> {
-  execute(input: I): Result<O, AppError>;
+  execute(input: I): Promise<Result<O, AppError>>;
 }
